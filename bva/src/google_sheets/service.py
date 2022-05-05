677e4979a0c2b0d087f61e2c98a91b483febc1ac
Andreas Bergem <bergem.andreas@gmail.com>
2022-05-05 15:07:20 +0200

    Return first name and last name of technicians

--- bva/src/google_sheets/service.py
+++ bva/src/google_sheets/service.py
@@ -6,19 +6,31 @@ from .sheets import read_spreadsheet
 
 def get_technicians_for_date(date: datetime.date) -> typing.Optional[typing.Dict[str, str]]:
     spreadshet_id = "1nNVrzcVQFULWVans5zt3N0_tqsl6U9U1hzGV_mhseJ4"
-    range = "A2:C"
+    schedule_range = "A2:C"
+    info_range = "E2:F13"
 
-    rows = read_spreadsheet(spreadshet_id, range)
+    schedule_rows = read_spreadsheet(spreadshet_id, schedule_range)
+    info_rows = read_spreadsheet(spreadshet_id, info_range)
     # Todo: Put model somewhere else
     technicians = {"PC": None, "Lyd": None}
 
-    for row in rows:
+    def find_info(name):
+        for row in info_rows:
+            if not row:
+                continue
+            first, last = row
+            if first is None or last is None:
+                continue
+            if first == name:
+                return f"{first} {last}"
+
+    for row in schedule_rows:
         if not row:
             continue
         value_date = datetime.datetime.strptime(row[0], "%d. %B %Y").date()
         if value_date == date:
             for key, value in zip(technicians, row[1:]):
                 if value:
-                    technicians[key] = value
+                    technicians[key] = find_info(value)
             return technicians
     return
