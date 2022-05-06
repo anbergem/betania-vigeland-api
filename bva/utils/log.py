665d1f37599db091b05a3fad25d9ed6b8489cdb6
Andreas Bergem <bergem.andreas@gmail.com>
2022-05-06 12:16:53 +0200

    Fix log formatter

--- bva/utils/log.py
+++ bva/utils/log.py
@@ -7,5 +7,5 @@ def initialize_logging(*, stdout_level=logging.INFO):
     log.setLevel(logging.DEBUG)
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(stdout_level)
-    stdout_handler.setFormatter(logging.Formatter("%(asctime) - %(name) - %(levelname) - %(message)"))
+    stdout_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
     log.addHandler(stdout_handler)
